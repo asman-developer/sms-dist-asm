235daa7d4ffd39f2fec38a4461f24a22a6550b50
Abdylreshit <acerkezov97@gmail.com>
2023-07-08 19:39:00 +0500

    fix

--- app/Console/Commands/GammuAsyncHandleCommand.php
+++ app/Console/Commands/GammuAsyncHandleCommand.php
@@ -1,177 +1,14 @@
 <?php
 
-// namespace App\Console\Commands;
-
-// use App\Models\Service;
-// use App\Models\SMS;
-// use App\Models\Usb;
-// use Illuminate\Console\Command;
-// use Symfony\Component\Process\Process;
-// use Spatie\Async\Pool;
-// use SplQueue;
-// use Symfony\Component\Process\Exception\ProcessFailedException;
-// use Throwable;
-
-// class GammuAsyncHandleCommand extends Command
-// {
-//     /**
-//      * The name and signature of the console command.
-//      *
-//      * @var string
-//      */
-//     protected $signature = 'gammu:async:handle';
-
-//     /**
-//      * The console command description.
-//      *
-//      * @var string
-//      */
-//     protected $description = 'Async Sms handle by gammu';
-
-//     /**
-//      * Execute the console command.
-//      *
-//      * @return mixed
-//      */
-//     public function handle()
-//     {
-//         $s = 180;
-//         $tick = 0;
-
-//         $usbList = Service::with('usbList')
-//             // ->where('name', "SHOP_OTP")
-//             ->orWhere('name', "ASMAN_MARKET_OTP")
-//             ->get()
-//             ->pluck('usbList')
-//             ->collapse()
-//             ->pluck('port_numbers')
-//             ->unique()
-//             ->collapse()
-//             ->shuffle()
-//             ->toArray();
-
-//         while($tick < 180) {
-
-//             $chunked = SMS::whereStatus(0)
-//                         ->whereNull('distribution_id')
-//                         ->where("created_at", '>', now()->subSeconds(110))
-//                         ->orderBy('id', 'desc')
-//                         ->get()
-//                         ->chunk(count($usbList));
-
-//             $usbQueue = $this->getUSBQueue($usbList);
-
-//             $pool = Pool::create()->concurrency(count($usbList));
-
-//             $chunked->each(function($pending) use ($usbQueue, $pool, $usbList){
-
-//                 foreach ($pending as $sms) {
-//                     if ($sms->created_at->diffInSeconds(now()) > 110 || $sms->tries >= 2) {
-//                         return;
-//                     }
-
-//                     $usbQueue = $usbQueue->isEmpty() ? $this->getUSBQueue($usbList) : $usbQueue;
-
-//                     $usbNum = $usbQueue->dequeue();
-
-//                     $smsCallback = $this->handleSMS($sms->phone, $sms->content, $usbNum);
-
-//                     $pool
-//                         ->add($smsCallback)
-//                         ->then(function ($output) use ($sms, $usbNum) {
-//                             // executes after the command finishes
-//                             $usbId = Usb::whereJsonContains('port_numbers', $usbNum)->first()->id;
-//                             $sms->status = 1;
-//                             $sms->usb_id = $usbId;
-//                             $sms->completed_at = now();
-//                             $sms->save();
-//                         })->catch(function (Throwable $exception) use ($sms, $usbNum) {
-//                             $this->info($exception->getMessage());
-//                             $usbId = Usb::whereJsonContains('port_numbers', $usbNum)->first()->id;
-//                             $sms->usb_id = $usbId;
-//                             $sms->tries = $sms->tries + 1;
-//                             $sms->save();
-//                         });
-//                 }
-                
-//                 $pool->wait();
-//             });
-
-//             if ($chunked->isEmpty()) {
-//                 sleep(5);
-//                 $tick = $tick + 5;
-//             }
-//         }
-//     }
-
-//     public function getUSBQueue(array $usbList) : SplQueue {
-//         $usbQueue = new SplQueue();
-
-//         foreach($usbList as $usb) {
-//             $usbQueue[] = $usb;
-//         }
-
-//         return $usbQueue;
-//     }
-
-//     public function handleSMS($phone, $content, $usbNum)
-//     {
-//         // 1, 2 - 99364765192
-//         // 3, 4 - 99364746854
-//         return function () use ($phone, $content, $usbNum) {
-//                 $process = new Process([
-//                     'gammu',
-//                     "-s",
-//                     $usbNum,
-//                     'sendsms',
-//                     'TEXT',
-//                     "+{$phone}",
-//                     // '-flash',
-//                     '-text',
-//                     // "{$content} - {$usbNum}"
-//                     "{$content}"
-//                 ]);
-
-//                 $process->run();
-
-//                 if (!$process->isSuccessful()) {
-//                     throw new ProcessFailedException($process);
-//                 }
-//         };
-//     }
-
-//     public function handleSMSFromShell($phone, $content, $usbNum)
-//     {
-//         $process = Process::fromShellCommandline('echo -e "$MESSAGE" | sudo gammu -s "$USBNUM" sendsms TEXT "$PHONE" -len "$LENGHT"');
-
-//         $process->run(null, [
-//             'MESSAGE' => $content,
-//             'PHONE'   => $phone,
-//             'USBNUM'  => $usbNum,
-//             'LENGHT'  => 400
-//         ]);
-
-//         $process->run();
-
-//         if (!$process->isSuccessful()) {
-//             throw new ProcessFailedException($process);
-//         }
-//     }
-// }
-
-
-
-
-// <?php
-
 namespace App\Console\Commands;
 
 use App\Models\Service;
 use App\Models\SMS;
+use App\Models\Usb;
 use Illuminate\Console\Command;
-use Illuminate\Support\Arr;
 use Symfony\Component\Process\Process;
 use Spatie\Async\Pool;
+use SplQueue;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Throwable;
 
@@ -202,65 +39,63 @@ class GammuAsyncHandleCommand extends Command
         $tick = 0;
 
         $usbList = Service::with('usbList')
-            ->where('name', "ASMAN_MARKET_OTP")
+            // ->where('name', "SHOP_OTP")
+            ->orWhere('name', "ASMAN_MARKET_OTP")
             ->get()
             ->pluck('usbList')
-            ->collapse();
-
-        $usbIds = $usbList->keyBy('port_numbers.0');
+            ->collapse()
+            ->pluck('port_numbers')
+            ->unique()
+            ->collapse()
+            ->shuffle()
+            ->toArray();
 
         while($tick < 180) {
 
+            $chunked = SMS::whereStatus(0)
+                        ->whereNull('distribution_id')
+                        ->where("created_at", '>', now()->subSeconds(110))
+                        ->orderBy('id', 'desc')
+                        ->get()
+                        ->chunk(count($usbList));
+
+            $usbQueue = $this->getUSBQueue($usbList);
+
             $pool = Pool::create()->concurrency(count($usbList));
 
-            releoadChunk:
-                $chunked = SMS::whereStatus(0)
-                    ->whereNull('distribution_id')
-                    ->where("created_at", '>', now()->subSeconds(110))
-                    ->orderBy('id', 'desc')
-                    ->get()
-                    ->chunk(count($usbList));
-
-            foreach ($chunked as $pending) {
-
-                $usbQueue = $this->getUSBQueue($usbList);
+            $chunked->each(function($pending) use ($usbQueue, $pool, $usbList){
 
                 foreach ($pending as $sms) {
                     if ($sms->created_at->diffInSeconds(now()) > 110 || $sms->tries >= 2) {
                         return;
                     }
 
-                    $usbNum = Arr::where($usbQueue, function ($value, $key) { return $value == 0; });
+                    $usbQueue = $usbQueue->isEmpty() ? $this->getUSBQueue($usbList) : $usbQueue;
 
-                    if (!count($usbNum)){
-                        $pool->wait();
-                        goto releoadChunk;
-                    }
-
-                    $usbNum = Arr::random(array_keys($usbNum));
-
-                    $usbQueue[$usbNum] = 1;
+                    $usbNum = $usbQueue->dequeue();
 
                     $smsCallback = $this->handleSMS($sms->phone, $sms->content, $usbNum);
 
                     $pool
                         ->add($smsCallback)
-                        ->then(function ($output) use ($sms, $usbNum, $usbIds) {
+                        ->then(function ($output) use ($sms, $usbNum) {
                             // executes after the command finishes
+                            $usbId = Usb::whereJsonContains('port_numbers', $usbNum)->first()->id;
                             $sms->status = 1;
-                            $sms->usb_id = $usbIds[$usbNum]->id;
+                            $sms->usb_id = $usbId;
                             $sms->completed_at = now();
                             $sms->save();
-                        })->catch(function (Throwable $exception) use ($sms, $usbNum, $usbIds, &$usbList) {
-                            $sms->usb_id = $usbIds[$usbNum]->id;
+                        })->catch(function (Throwable $exception) use ($sms, $usbNum) {
+                            $this->info($exception->getMessage());
+                            $usbId = Usb::whereJsonContains('port_numbers', $usbNum)->first()->id;
+                            $sms->usb_id = $usbId;
                             $sms->tries = $sms->tries + 1;
                             $sms->save();
-                            $usbList = $usbList->reject(fn($v, $k) => $v->id == $usbIds[$usbNum]->id);
                         });
                 }
                 
                 $pool->wait();
-            }
+            });
 
             if ($chunked->isEmpty()) {
                 sleep(5);
@@ -269,17 +104,11 @@ class GammuAsyncHandleCommand extends Command
         }
     }
 
-    public function getUSBQueue($usbList){
-        $usbQueue=null;
+    public function getUSBQueue(array $usbList) : SplQueue {
+        $usbQueue = new SplQueue();
 
-        $list = $usbList
-            ->pluck('port_numbers')
-            ->unique()
-            ->collapse()
-            ->shuffle();
-
-        foreach($list as $usb) {
-            $usbQueue[$usb] = 0;
+        foreach($usbList as $usb) {
+            $usbQueue[] = $usb;
         }
 
         return $usbQueue;
@@ -329,3 +158,175 @@ class GammuAsyncHandleCommand extends Command
         }
     }
 }
+
+
+
+
+// <?php
+
+// namespace App\Console\Commands;
+
+// use App\Models\Service;
+// use App\Models\SMS;
+// use Illuminate\Console\Command;
+// use Illuminate\Support\Arr;
+// use Symfony\Component\Process\Process;
+// use Spatie\Async\Pool;
+// use Symfony\Component\Process\Exception\ProcessFailedException;
+// use Throwable;
+
+// class GammuAsyncHandleCommand extends Command
+// {
+//     /**
+//      * The name and signature of the console command.
+//      *
+//      * @var string
+//      */
+//     protected $signature = 'gammu:async:handle';
+
+//     /**
+//      * The console command description.
+//      *
+//      * @var string
+//      */
+//     protected $description = 'Async Sms handle by gammu';
+
+//     /**
+//      * Execute the console command.
+//      *
+//      * @return mixed
+//      */
+//     public function handle()
+//     {
+//         $s = 180;
+//         $tick = 0;
+
+//         $usbList = Service::with('usbList')
+//             ->where('name', "ASMAN_MARKET_OTP")
+//             ->get()
+//             ->pluck('usbList')
+//             ->collapse();
+
+//         $usbIds = $usbList->keyBy('port_numbers.0');
+
+//         while($tick < 180) {
+
+//             $pool = Pool::create()->concurrency(count($usbList));
+
+//             releoadChunk:
+//                 $chunked = SMS::whereStatus(0)
+//                     ->whereNull('distribution_id')
+//                     ->where("created_at", '>', now()->subSeconds(110))
+//                     ->orderBy('id', 'desc')
+//                     ->get()
+//                     ->chunk(count($usbList));
+
+//             foreach ($chunked as $pending) {
+
+//                 $usbQueue = $this->getUSBQueue($usbList);
+
+//                 foreach ($pending as $sms) {
+//                     if ($sms->created_at->diffInSeconds(now()) > 110 || $sms->tries >= 2) {
+//                         return;
+//                     }
+
+//                     $usbNum = Arr::where($usbQueue, function ($value, $key) { return $value == 0; });
+
+//                     if (!count($usbNum)){
+//                         $pool->wait();
+//                         goto releoadChunk;
+//                     }
+
+//                     $usbNum = Arr::random(array_keys($usbNum));
+
+//                     $usbQueue[$usbNum] = 1;
+
+//                     $smsCallback = $this->handleSMS($sms->phone, $sms->content, $usbNum);
+
+//                     $pool
+//                         ->add($smsCallback)
+//                         ->then(function ($output) use ($sms, $usbNum, $usbIds) {
+//                             // executes after the command finishes
+//                             $sms->status = 1;
+//                             $sms->usb_id = $usbIds[$usbNum]->id;
+//                             $sms->completed_at = now();
+//                             $sms->save();
+//                         })->catch(function (Throwable $exception) use ($sms, $usbNum, $usbIds, &$usbList) {
+//                             $this->info($exception->getMessage());
+//                             $sms->usb_id = $usbIds[$usbNum]->id;
+//                             $sms->tries = $sms->tries + 1;
+//                             $sms->save();
+//                             $usbList = $usbList->reject(fn($v, $k) => $v->id == $usbIds[$usbNum]->id);
+//                         });
+//                 }
+                
+//                 $pool->wait();
+//             }
+
+//             if ($chunked->isEmpty()) {
+//                 sleep(5);
+//                 $tick = $tick + 5;
+//             }
+//         }
+//     }
+
+//     public function getUSBQueue($usbList){
+//         $usbQueue=null;
+
+//         $list = $usbList
+//             ->pluck('port_numbers')
+//             ->unique()
+//             ->collapse()
+//             ->shuffle();
+
+//         foreach($list as $usb) {
+//             $usbQueue[$usb] = 0;
+//         }
+
+//         return $usbQueue;
+//     }
+
+//     public function handleSMS($phone, $content, $usbNum)
+//     {
+//         // 1, 2 - 99364765192
+//         // 3, 4 - 99364746854
+//         return function () use ($phone, $content, $usbNum) {
+//                 $process = new Process([
+//                     'gammu',
+//                     "-s",
+//                     $usbNum,
+//                     'sendsms',
+//                     'TEXT',
+//                     "+{$phone}",
+//                     // '-flash',
+//                     '-text',
+//                     // "{$content} - {$usbNum}"
+//                     "{$content}"
+//                 ]);
+
+//                 $process->run();
+
+//                 if (!$process->isSuccessful()) {
+//                     throw new ProcessFailedException($process);
+//                 }
+//         };
+//     }
+
+//     public function handleSMSFromShell($phone, $content, $usbNum)
+//     {
+//         $process = Process::fromShellCommandline('echo -e "$MESSAGE" | sudo gammu -s "$USBNUM" sendsms TEXT "$PHONE" -len "$LENGHT"');
+
+//         $process->run(null, [
+//             'MESSAGE' => $content,
+//             'PHONE'   => $phone,
+//             'USBNUM'  => $usbNum,
+//             'LENGHT'  => 400
+//         ]);
+
+//         $process->run();
+
+//         if (!$process->isSuccessful()) {
+//             throw new ProcessFailedException($process);
+//         }
+//     }
+// }
